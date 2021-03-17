c596aecba052448417a395b4fec09a3902b63734
Waterticket <matthew218@naver.com>
2021-03-17 12:35:41 +0900

    기본 파일들 커밋

---
+++ cmake-build-debug/CMakeFiles/cordelia_db.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cordelia_db.dir/main.cpp.obj"
+  "cordelia_db.exe"
+  "cordelia_db.exe.manifest"
+  "cordelia_db.pdb"
+  "libcordelia_db.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cordelia_db.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
