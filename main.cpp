9f8de87628ebd3f028bdda87e28bd84501f93e74
Waterticket <matthew218@naver.com>
2021-03-17 12:08:55 +0900

    WHERE 추가된 SQL문, main.cpp에서 table 출력

--- main.cpp
+++ main.cpp
@@ -4,6 +4,16 @@
 
 int main() {
     DBHandler dh;
-    dh.executeQuery("SELECT a,b,c FROM asdf lol");
+    ResultSet rs = dh.executeQuery("SELECT a,b,c FROM dbtest WHERE index=2");
+
+    for(int i = 0; i < rs.table_tuples.size(); i++)
+    {
+        for(int j = 0; j < rs.table_tuples[i].size(); j++)
+        {
+            std::cout << rs.table_tuples[i][j] << " ";
+        }
+
+        std::cout << std::endl;
+    }
     return 0;
 }
