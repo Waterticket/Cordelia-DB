758859080365750404eb2a90a5954b5ca45ef108
Waterticket <matthew218@naver.com>
2021-03-17 12:05:50 +0900

    ResultSet 기본 구조

--- framework/DB/ResultSet.h
+++ framework/DB/ResultSet.h
@@ -6,7 +6,12 @@
 #define CORDELIA_DB_RESULTSET_H
 
 class ResultSet {
+public:
+    std::vector< std::string > column_name;
+    std::vector< std::string > column_type; //int, string, etc..
+    std::vector< std::string > column_opt; // PK(primary_key), AI(auto_increment), etc..
 
+    std::vector< std::vector < std::string > > table_tuples; // data
 };
 
 #endif //CORDELIA_DB_RESULTSET_H
